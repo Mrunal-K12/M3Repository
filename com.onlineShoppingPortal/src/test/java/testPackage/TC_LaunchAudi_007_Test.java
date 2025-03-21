ba9df864a8e01f1763b6ffce9a523fcb50d8e852
Admin <Admin@DESKTOP-L7NO5ET>
2025-03-21 12:10:27 +0530

    Second Commit Version 0.2

---
+++ com.onlineShoppingPortal/src/test/java/testPackage/TC_LaunchAudi_007_Test.java
@@ -0,0 +1,20 @@
+package testPackage;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TC_LaunchAudi_007_Test {
+  @Test
+  public void launchAudi() throws InterruptedException {
+	  WebDriver driver = new ChromeDriver();
+		 driver.manage().window().maximize();
+		 driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+		 driver.get("https://www.audi.in/");
+		 
+		 Thread.sleep(1000);
+		 driver.quit();
+  }
+}
---
+++ com.onlineShoppingPortal/src/test/java/testPackage/TC_LaunchTata_005_Test.java
@@ -0,0 +1,20 @@
+package testPackage;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TC_LaunchTata_005_Test {
+  @Test
+  public void launchTataMotors() throws InterruptedException {
+	  WebDriver driver = new ChromeDriver();
+		 driver.manage().window().maximize();
+		 driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+		 driver.get("https://www.tatamotors.com/");
+		 
+		 Thread.sleep(1000);
+		 driver.quit();
+  }
+}
